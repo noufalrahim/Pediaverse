1102422d12e2860becd1a1d058731372d4495933
noufalrahim <noufalrahim6784@gmail.com>
2025-03-16 14:34:11 +0530

    feat: added student and education types

---
+++ src/types/StudentType.ts
@@ -0,0 +1,11 @@
+export type StudentType = {
+    id?: string;
+    name: string;
+    rollNo: string;
+    course: string;
+    age: number;
+    mail: string;
+    phoneNumber: string;
+    courseYear?: number;
+    location: string;
+};
